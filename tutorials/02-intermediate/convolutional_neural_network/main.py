d62191b9999f554e5af9e355b38d9228e5b2748a
zhangyang1997 <87896052@qq.com>
2020-06-09 15:23:43 +0800

    up

--- tutorials/02-intermediate/convolutional_neural_network/main.py
+++ tutorials/02-intermediate/convolutional_neural_network/main.py
@@ -1,38 +1,38 @@
-import torch 
+import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
 
-# Device configuration
+# 设备配置
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# Hyper parameters
+# 超参数
 num_epochs = 5
 num_classes = 10
 batch_size = 100
 learning_rate = 0.001
 
-# MNIST dataset
+# MNIST数据集
 train_dataset = torchvision.datasets.MNIST(root='../../data/',
-                                           train=True, 
+                                           train=True,
                                            transform=transforms.ToTensor(),
                                            download=True)
 
 test_dataset = torchvision.datasets.MNIST(root='../../data/',
-                                          train=False, 
+                                          train=False,
                                           transform=transforms.ToTensor())
 
-# Data loader
+# 数据加载器
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
+                                           batch_size=batch_size,
                                            shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
+                                          batch_size=batch_size,
                                           shuffle=False)
 
-# Convolutional neural network (two convolutional layers)
+# 卷积神经网络(两个卷积层)
 class ConvNet(nn.Module):
     def __init__(self, num_classes=10):
         super(ConvNet, self).__init__()
@@ -47,7 +47,7 @@ class ConvNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc = nn.Linear(7*7*32, num_classes)
-        
+
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
@@ -55,34 +55,35 @@ class ConvNet(nn.Module):
         out = self.fc(out)
         return out
 
+
 model = ConvNet(num_classes).to(device)
 
-# Loss and optimizer
+# 损失和优化器
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# Train the model
+# 训练模型
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         images = images.to(device)
         labels = labels.to(device)
-        
-        # Forward pass
+
+        # 前向计算
         outputs = model(images)
         loss = criterion(outputs, labels)
-        
-        # Backward and optimize
+
+        # 误差反向传播和优化
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-# Test the model
-model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+        if (i+1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+# 测试模型
+model.eval()  # 评价模式(batch norm uses moving mean/variance instead of mini-batch mean/variance)
 with torch.no_grad():
     correct = 0
     total = 0
@@ -94,7 +95,8 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format(
+        100 * correct / total))
 
-# Save the model checkpoint
+# 保存模型checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
